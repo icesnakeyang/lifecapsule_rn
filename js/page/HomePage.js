1e3e6d8d85a170a9178e495fee6ef73a936cc5d4
icesnakeyang <flow_ocean@yahoo.com>
2019-09-20 10:58:34 +0800

    增加对Android回退键的处理

--- js/page/HomePage.js
+++ js/page/HomePage.js
@@ -1,16 +1,42 @@
 import React, {Component} from 'react'
-import {
-    View
-} from 'react-native'
+import {NavigationActions} from "react-navigation";
 import DynamicTabNavigator from "../navigator/DynamicTabNavigator";
 import NavigationUtil from "../navigator/NavigationUtil";
+import {connect} from "react-redux";
+import {BackHandler} from 'react-native'
 
-export default class HomePage extends Component {
+class HomePage extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    componentDidMount() {
+        BackHandler.addEventListener('hardwareBackPress', ()=>{
+            const {nav, dispatch} =this.props
+            if (nav.routes[1].index === 0) {//如果RootNavigator中的MainNavigator的index为0，则不处理返回事件
+                return false;
+            }
+            dispatch(NavigationActions.back());
+            return true;
+        })
+    }
+
+    componentWillUnmount() {
+        BackHandler.removeEventListener("hardwareBackPress")
+    }
+
     render() {
         NavigationUtil.navigation = this.props.navigation
         return (
             <DynamicTabNavigator/>
         )
     }
-
 }
+
+
+const mapStateToProps = state => ({
+    nav: state.nav
+});
+
+export default connect(mapStateToProps)(HomePage);
